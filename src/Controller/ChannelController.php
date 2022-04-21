77722dc46deb2e918ccd389b94b1b6692b5c86bf
A.Hassaine <ab.hassaine@protonmail.com>
2022-04-21 13:56:16 +0100

    creation of Channel & Message controllers

---
+++ src/Controller/ChannelController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Channel;
+use App\Repository\ChannelRepository;
+use App\Repository\MessageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(name: 'channel_')]
+class ChannelController extends AbstractController
+{
+    public function __construct(
+        private readonly ChannelRepository $channelRepository,
+        private readonly MessageRepository $messageRepository
+    )
+    {
+    }
+
+    #[Route('/', name: 'home')]
+    public function getChannels(): Response
+    {
+        $channels = $this->channelRepository->findAll();
+
+        return $this->render('channel/index.html.twig', ['channels' => $channels ?? []]);
+    }
+
+    #[Route('/chat', name: 'chat')]
+    public function chat(Channel $channel): Response
+    {
+        $messages = $this->messageRepository->findBy([
+            'channel' => $channel
+        ], ['createdAt' => 'ASC']);
+
+        return $this->render('channel/chat.html.twig', compact('channel', 'messages'));
+    }
+}
